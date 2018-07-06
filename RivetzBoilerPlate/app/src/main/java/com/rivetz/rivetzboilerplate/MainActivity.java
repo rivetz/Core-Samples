b5c1f3850ee202f8ebf3d62dec347e4e9d385826
Nicolas Schapeler <nicoschap@live.fr>
2018-07-06 13:55:24 -0700

    fixed some formatting and removed an unnecessary null-check

--- RivetzBoilerPlate/app/src/main/java/com/rivetz/rivetzboilerplate/MainActivity.java
+++ RivetzBoilerPlate/app/src/main/java/com/rivetz/rivetzboilerplate/MainActivity.java
@@ -13,22 +13,20 @@ import com.rivetz.bridge.Rivet;
 public class MainActivity extends AppCompatActivity {
     Rivet rivet;
 
-    //creates and pairs a Rivet if necessary
+    // Creates and pairs a Rivet if necessary
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         rivet = new Rivet(this, SPID.DEVELOPER_TOOLS_SPID);
 
-        if (rivet != null) {
             if (!rivet.isPaired()) {
                 rivet.pairDevice(this);
             }
-        }
 
     }
 
-    //checks if the Rivet was successfully paired
+    // Checks if the Rivet was successfully paired
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
@@ -45,63 +43,52 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-
-
-    //creates a Key
+    // Creates a Key
     public void createKey(View v) {
-        //create a Key using the Rivet
+        // Create a Key using the Rivet
     }
 
-    //Helper functions
+    // Helper functions
 
-    //creates an alert with some text
+    // Creates an alert with some text
     public void alert(String text) {
         new AlertDialog.Builder(this)
                 .setMessage(text)
                 .create().show();
     }
 
-    //makes a button unclickable
+    // Makes a button unclickable
     public void makeUnclickable(Button button){
         button.setAlpha(.5f);
         button.setClickable(false);
     }
 
-    //makes a button clickable
+    // Makes a button clickable
     public void makeClickable(Button button){
         button.setAlpha(1f);
         button.setClickable(true);
     }
 
-    //shows a loading animation
+    // Shows a loading animation
     public void loading(){
         findViewById(R.id.loading).setVisibility(View.VISIBLE);
     }
 
-    //makes the loading animation invisible
+    // Makes the loading animation invisible
     public void notLoading(){
         findViewById(R.id.loading).setVisibility(View.GONE);
     }
 
 
-    //converts a byte array to hexadecimals
+    // Converts a byte array to hexadecimals
     public static String bytesToHex(byte[] bytes) {
         final char[] hexArray = "0123456789ABCDEF".toCharArray();
         char[] hexChars = new char[bytes.length * 2];
-
         for ( int j = 0; j < bytes.length; j++ ) {
-
             int v = bytes[j] & 0xFF;
-
             hexChars[j * 2] = hexArray[v >>> 4];
-
             hexChars[j * 2 + 1] = hexArray[v & 0x0F];
-
         }
-
-
         return new String(hexChars);
-
     }
-
 }
