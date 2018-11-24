a09f4cc832490360a9a30f689258d691193831ae
Nicolas Schapeler <nicoschap@live.fr>
2018-11-23 17:15:54 -0800

    removed the backgroundapps

--- BackgroundServiceApps/CalcService/app/src/main/java/com/rivetz/calcservice/MainActivity.java
+++
@@ -1,6 +0,0 @@
-package com.rivetz.calcservice;
-
-import android.support.v7.app.AppCompatActivity;
-
-public class MainActivity extends AppCompatActivity {
-}
--- BackgroundServiceApps/README.md
+++
@@ -1,2 +0,0 @@
-# Backgroundservice apps
-These apps demonstrate work with background services in Android. They are not directly related to Rivetz.
