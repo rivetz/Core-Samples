d34732a48a875175e6e4958d3a7fb0802b8a77aa
Nicolas Schapeler <nicoschap@live.fr>
2018-12-06 17:28:07 -0800

    Boilerplate code and signingsample updates

--- RivetzBoilerPlate/app/src/main/java/com/rivetz/rivetzboilerplate/MainActivity.java
+++ RivetzBoilerPlate/app/src/main/java/com/rivetz/rivetzboilerplate/MainActivity.java
@@ -1,41 +1,30 @@
-/*******************************************************************************
- *
- * RIVETZ CORP. CONFIDENTIAL
- *__________________________
- *
- * Copyright (c) 2018 Rivetz Corp.
- * All Rights Reserved.
- *
- * All information and intellectual concepts contained herein is, and remains,
- * the property of Rivetz Corp and its suppliers, if any.  Dissemination of this
- * information or reproduction of this material, or any facsimile, is strictly
- * forbidden unless prior written permission is obtained from Rivetz Corp.
- ******************************************************************************/
 package com.rivetz.rivetzboilerplate;
 
 import android.os.Bundle;
 import android.support.v7.app.AlertDialog;
 import android.view.View;
 import android.widget.Button;
+
 import com.rivetz.api.RivetCrypto;
+import com.rivetz.api.RivetRuntimeException;
 import com.rivetz.api.SPID;
 import com.rivetz.bridge.RivetWalletActivity;
+
+import java.util.concurrent.ExecutionException;
 
 public class MainActivity extends RivetWalletActivity {
     private RivetCrypto crypto;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
-        // Starts the Rivet lifecycle with the Activity
+        // Starts the Rivet lifecycle with the Activity and sets the UI
         super.onCreate(savedInstanceState);
-
         setContentView(R.layout.activity_main);
         loading();
 
+
         // Start the pairing process
         pairDevice(SPID.DEVELOPER_TOOLS_SPID).whenComplete((paired, ex) -> {
-
             runOnUiThread(() -> {
                 notLoading();
             });
@@ -43,16 +32,20 @@ public class MainActivity extends RivetWalletActivity {
             if (paired != null) {
 
                 if (paired.booleanValue()) {
-                    crypto = getRivetCrypto();
-                    if (crypto != null) {
+
+                    try {
+                        crypto = getRivetCrypto();
                         runOnUiThread(() -> {
                             onDevicePairing(true);
                         });
-                    } else {
+                    }
+
+                    catch (RivetRuntimeException e) {
                         runOnUiThread(() -> {
-                            onDevicePairing(false);
+                            alert(e.getError().getMessage());
                         });
                     }
+
                 } else  {
                     runOnUiThread(() -> {
                         onDevicePairing(false);
@@ -70,12 +63,12 @@ public class MainActivity extends RivetWalletActivity {
     public void onDevicePairing(boolean success){
         if (success) {
             alert("Paired");
-            //Do something
         } else {
             alert("Pairing error!");
-            //Do something different
         }
+        notLoading();
     }
+
 
     // Helper functions
 
