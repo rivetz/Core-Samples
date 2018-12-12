9154c7ab5600639d686c8579abaa3e680ff342dd
Nicolas Schapeler <38372048+nschapeler@users.noreply.github.com>
2018-12-11 16:17:54 -0800

    Changed wrong 'true' to 'false' in function call

--- EncryptDecryptSample/app/src/main/java/com/rivetz/r_app2/MainActivity.java
+++ EncryptDecryptSample/app/src/main/java/com/rivetz/r_app2/MainActivity.java
@@ -48,7 +48,7 @@ public class MainActivity extends RivetWalletActivity {
                         }
                         else {
                             runOnUiThread(() -> {
-                                onDevicePairing(true);
+                                onDevicePairing(false);
                             });
                         }
                     }
